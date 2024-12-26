7fbd8a1c9052b37456bda24af72191d1340add8b
OrpheusGr <hellaras7@gmail.com>
2024-12-26 01:58:28 +0000

    Updates

--- ircbot.py
+++ ircbot.py
@@ -9,8 +9,24 @@ settings = json.loads(f.read())
 f.close()
 irc_settings = settings["irc"]
 
+def save_settings(settings):
+    file = "settings.json"
+    with open(file, 'w') as json_file:
+        json.dump(settings, json_file)
+
+def on_join(connection, event):
+    global settings
+    if event.target not in settings["irc"]["channels"]:
+        settings["irc"]["channels"].append(event.target)
+        save_settings(settings)
+        f = open("settings.json", encoding="utf-8")
+        settings = json.loads(f.read())
+        f.close()
+        irc_settings = settings["irc"]
+        print(irc_settings)
 
 def on_connect(connection, event):
+    global irc_settings
     print("Connected successfully")
     for i in irc_settings["channels"]:
         connection.join(i)
@@ -32,13 +48,14 @@ def startirct():
     c = react.server().connect(irc_settings["server"], irc_settings["port"], irc_settings["nickname"], None, "weather", "I do them weather stuff")
     c.add_global_handler("welcome", on_connect)
     c.add_global_handler("pubmsg", on_pubmsg)
+    c.add_global_handler("join", on_join)
     loopin = 1
     while loopin:
-        try:
-            time.sleep(0.2)
-            react.process_once(0.2)
-        except Exception as err:
-            print("An error has occured. %s" % str(err))
-            pass
+        #try:
+        time.sleep(0.2)
+        react.process_once(0.2)
+        #except Exception as err:
+           # print("An error has occured. %s" % str(err))
+            #pass
 
 startirct()
